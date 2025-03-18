9a013954efe3c7938f8ead6e94ebf2930361ab78
Reginaldo L. Silva <90699477+Regis-Gold@users.noreply.github.com>
2025-03-17 21:23:38 -0300

    Uso de constantes em JS

---
+++ 4-constantes.js
@@ -0,0 +1,25 @@
+// const para criar váriaveis
+// Não podemos criar contantes com palavras reservadas da linguagem JS.
+// Contantes precisam ter nomes significativos
+// Não pode começar o nome da contantes com número
+// Não podem conter espaçoes ou traços
+// Utilizamos camelCase
+// Essas variáveis são Case-sensitive
+// Não podemos redeclarar contantes com const
+// Não pode modificar o valor de uma contante
+// Não utilizar var, utilizar const.
+// Não pode declarar const sem inicializar ela
+
+// + - * /
+
+const primeiroNumero = 5;
+const segundoNumero = 10;
+const resultado = primeiroNumero * segundoNumero;
+console.log(resultado);
+const resultadoDuplicado = resultado * 2;
+let resultadoTriplicado = resultado * 3;
+resultadoTriplicado = resultadoTriplicado + 40;
+console.log(resultadoTriplicado);
+console.log(typeof primeiroNumero);
+
+// CTRL + K + S Atalhos de teclado
