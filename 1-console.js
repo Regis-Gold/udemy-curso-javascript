9c940ade752a3cf60fa01fd33d2e50ba48434baa
Reginaldo L. Silva <90699477+Regis-Gold@users.noreply.github.com>
2025-03-17 21:19:58 -0300

    Console log no navegador

---
+++ 1-console.js
@@ -0,0 +1,13 @@
+
+// console.log() para exibir somente no console
+// Para usar aspas simples ou duplas
+// Toda String = Texto
+/* Number = Número */
+console.log("Olá Imundo");
+console.log('"Reginaldo" Luiz'); 
+console.log("Reginaldo Luiz");
+console.log(`Reginaldo 'Luiz'`);
+
+console.log(1234856);
+console.log(15.85); //. inves de , em casa decimal
+console.log(100, "Reginaldo");
