f29aacc53ee73e533a5c3244ce7fed487778dbb7
Reginaldo L. Silva <90699477+Regis-Gold@users.noreply.github.com>
2025-03-17 21:23:00 -0300

    Variáveis em JavaScript

---
+++ 3-variaveis.js
@@ -0,0 +1,32 @@
+// let para criar váriaveis
+// Não podemos criar variáveis com palavras reservadas da linguagem JS.
+// Variáveis precisam ter nomes significativos
+// Não pode começar o nome da váriavel com número
+// Não podem conter espaçoes ou traços
+// Utilizamos camelCase
+// Essas variáveis são Case-sensitive
+// Não podemos redeclarar variáveis com let
+// Não utilizar var, utilizar let
+
+let nome; // Declarar variável
+nome = 'Reginaldo'; // Inicializando variável
+console.log(nome);
+nome = 'Luiz'
+console.log(nome);
+
+
+/*
+console.log(nome, 'nasceu em 1998.');
+console.log('Em 2015', nome, 'conheceu Maria.');
+console.log(nome, 'casou-se com Maria em 2020.');
+console.log('Maria teve um filho com', nome, 'em 2021.');
+console.log('O filho e', nome, 'se chama Eduardo.');
+*/
+
+/*
+console.log('Tudo o que um ');
+console.log('sonho precisa');
+console.log('para ser realizado');
+console.log(' é alguém que acredite');
+console.log('que ele possa ser realizado.');
+*/
