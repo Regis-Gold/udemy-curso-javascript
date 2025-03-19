9e003c7b234b4a7bcb06ef6caee325bad21413d5
Reginaldo L. Silva <90699477+Regis-Gold@users.noreply.github.com>
2025-03-18 21:57:45 -0300

    Uso correto de var e let

---
+++ 6-var-let.js
@@ -0,0 +1,10 @@
+//ECMAScript 2015 (ES6)
+// Não pode redeclarar variável usando var mas isso é errado
+// Não criar variável sem const ou sem let
+var nome = 'Reginaldo';
+var nome = 'Luiz'; // NÃO PODE REDECLAR !!!
+
+console.log(nome);
+
+sobrenome = 'Silva'; // NÃO FAZER ISSO !!!
+console.log(sobrenome);
