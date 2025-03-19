ab14b6a27b1cc5de42c211036472b8eda758cda9
Reginaldo L. Silva <90699477+Regis-Gold@users.noreply.github.com>
2025-03-18 21:58:16 -0300

    Dados primitivos em JS

---
+++ 7-dados-primitivos.js
@@ -0,0 +1,26 @@
+// String
+const nome = 'Reginaldo';
+const nome1 = "Reginaldo";
+const nome2 = `Reginaldo`;
+
+// Number
+const num1 = 10;
+const num2 = 10.50;
+
+// Undefined não aponta para lugar nenhum na memória
+let nomeAluno;
+
+// Null não aponta para lugar nenhum na memória
+const sobrenomeAluno = null;
+
+// Boolean = true or false (lógico)
+const aprovado = true;
+
+console.log (typeof aprovado, aprovado);
+
+let a = 2;
+const b = a;
+console.log(a, b);
+
+a = 3;
+console.log(a, b);
