893126c7f524381f48222460211c216718a8441b
Reginaldo L. Silva <90699477+Regis-Gold@users.noreply.github.com>
2025-03-23 20:52:44 -0300

    Uso dos operadores de comparação

---
+++ 21-operadores-comparacao.js
@@ -0,0 +1,12 @@
+// Operadores de comparação
+
+let a = 100;
+let b = 50;
+
+console.log(a > b); // > maior que
+console.log(a >= b); // >= maior que ou igual a
+console.log(a < b); // < menor que
+console.log(a <= b); // <= menor que ou igual a
+console.log(a === b); // === igualdade estrita (valor e tipo)
+console.log(a !== b); /* !== diferente estrito (valor e tipo) */
+
