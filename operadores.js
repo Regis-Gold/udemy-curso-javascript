5707415269fb44db53d4cf2c633869eae2b9eb22
Reginaldo L. Silva <90699477+Regis-Gold@users.noreply.github.com>
2025-04-17 22:41:06 -0300

    Operadores em JS

---
+++ operadores.js
@@ -0,0 +1,19 @@
+
+// Definições de alguns operadores em JavaScript //
+
+const a = 20;
+const b = 10;
+console.log(a + b); // adição
+console.log(a - b); // subtração
+console.log(a * b); // multiplicação
+console.log(a / b); // divisão
+console.log(a ** b); // elevado
+console.log(a % b); // módulo
+console.log(a > b); // maior que
+console.log(a < b); // menor que
+console.log(a === b); // igual (equivalência)
+console.log(a <= b); // menor ou igual
+console.log(a >= b); // maior ou igual
+console.log(a != b); // diferente de
+console.log(a && b); // operador lógico AND
+console.log(a || b); // operador lógico OR
